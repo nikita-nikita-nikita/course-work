80e9c6eda7e14ffa3f5cddbc0ab686fa426423c7
Nikita <neymarenko123@gmail.com>
2020-05-23 18:54:29 +0300

    delete mongo db

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const fs = require('fs');
 const bodyParser = require("body-parser");
 const path = require("path");
-const mongoose = require("mongoose");
-const {Schema, model} = require("mongoose");
 const app = express();
-const urlMDB = process.env.MONGODB_URL||"mongodb+srv://Nikita:fx98gu90@highscores-lxbtb.mongodb.net/highscores";
 const port = process.env.PORT||3000;
 async function start() {
     try {
-        await mongoose.connect(urlMDB, {useNewUrlParser:true,  useUnifiedTopology: true});
         app.listen(port, ()=>{
             console.log("server running port: " + port)
         });
@@ -16,42 +13,19 @@ async function start() {
         console.log(e);
     }
 }
-const highscores = new Schema({
-    name:String,
-    score:Number
-});
-const HighScores = model("Highscores", highscores);
 const publicDir = path.resolve(__dirname, "../public");
 app.use(express.static(publicDir));
 app.use(bodyParser.json());
 
 app.get("/highscores", async (req, res) => {
-    const result = await HighScores.find({});
-    res.send({highscores:result});
+    fs.readFile('highscores.json', (err, buffer) => {
+        let data = JSON.parse(buffer.toString());
+        res.send({highscores:data});
+    });
 });
 
 app.post("/gethighsores",async ({body:{highscores}}) => {
-    const result = await HighScores.find({});
-    for (let i = 0; i < result.length; i++) {
-        await HighScores.remove({_id: result[i]._id}, (error)=>{
-            if(error){
-                console.log(error);
-            }
-        });
-    }
     console.log(highscores);
-    try {
-        for (let i = 0; i < highscores.length; i++) {
-            const record = new HighScores({
-                name:highscores[i].name,
-                score: highscores[i].score
-            });
-            await record.save();
-        }
-
-    }catch (error) {
-        console.log(error)
-    }
-
+    fs.writeFileSync("highscores.json", JSON.stringify(highscores));
 });
 start();
